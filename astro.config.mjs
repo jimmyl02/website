6bef827a549dafc1f822ffb2986c9c340820e76a
jimmyl02 <jimmyli2002@gmail.com>
2025-12-28 09:14:09 -0800

    feat: sharp and setup sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jimmyli.us",
   vite: { plugins: [tailwindcss()] },
   integrations: [sitemap()],
 });
